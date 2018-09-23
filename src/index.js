d3a42230e8c3164cd38f1cb13c0d086ca730decb
Nik <nik-pavlovich@mail.ru>
2018-09-23 22:50:45 +0300

    100p/100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+
+  var lim = 11; // 5^11 = 48828125
+
+  var res = 0;
+ 
+ for(var i = 1; i < lim+1; i++){
+   
+  var temp = 0;
+  
+  temp = Math.floor(number / Math.pow(5, i));
+
+  if(temp === 0){
+    break;
+  } else {
+    res += temp;
+  }  
+ }
+
+ 
+  return res; 
+
 }
